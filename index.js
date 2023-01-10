d3787b55502ae134a55841474a0d11c51983b173
david zambrano corral <Yayoamigo>
2023-01-10 17:37:13 -0500

    Add pipe

--- index.js
+++ index.js
@@ -1,18 +1,46 @@
 const http = require('http')
 
-const PORT = 3000;
+const PORT = 3003;
 
 const server = http.createServer()
+
+const friends = [
+    {
+      id: 0,
+      name: 'Nikola Tesla',
+    },
+    {
+      id: 1,
+      name: 'Sir Isaac Newton',
+    },
+    {
+      id: 2,
+      name: 'Albert Einstein',
+    }
+  ];
+  
     
 server.on('request',(req,res) => {
     const items = req.url.split('/');
-    if (items[1] === 'friends'){
+
+    if (req.method === 'POST' && items[1] === 'friends'){
+        req.on('data', (data) => {
+            const friend = data.toString();
+            console.log('Request', friend);
+            friends.push(JSON.parse(friend))
+        });
+        req.pipe(res)
+    }
+    else if (req.method === 'GET' && items[1] === 'friends'){
     res.statusCode = 200;
     res.setHeader('Content-Type' , 'application/json')
-    res.end(JSON.stringify({
-        id: 1,
-        name: 'David yayo locorrr',
-    }))
+    if (items.length === 3){
+        const friendIndex = Number(items[2]);
+        res.end(JSON.stringify(friends[friendIndex]))
+    } else {
+        res.end(JSON.stringify(friends))
+    }
+    
 }else if (items[1] === 'messages'){
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>')
@@ -24,7 +52,8 @@ server.on('request',(req,res) => {
     res.write('</html>')
     res.end();
 } else {
-    res.statusCode = 404
+    res.statusCode = 404;
+    res.end()
 }
 
 })
